62f95449e95cefde4eec16bbe11baa262ade86db
Priscilla <mycilla74@gmail.com>
2019-02-15 11:25:51 +0000

    fixed jquery font colour change

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
 $(document).ready(() => {
-	$(".image p").on("mouseenter", () => {
-		$(".image p").css("color", "#bc9b07");
+	$(".image1").on("mouseenter", () => {
+		$(".image1 p").css("color", "#bc9b07");
 	})
-	$(".image p").on("mouseleave", () => {
-		$(".image p").css("color", "#e23a24");
+	$(".image1").on("mouseleave", () => {
+		$(".image1 p").css("color", "#e23a24");
+	})
+	$(".image2").on("mouseenter", () => {
+		$(".image2 p").css("color", "#bc9b07");
+	})
+	$(".image2").on("mouseleave", () => {
+		$(".image2 p").css("color", "#e23a24");
+	})
+	$(".image3").on("mouseenter", () => {
+		$(".image3 p").css("color", "#bc9b07");
+	})
+	$(".image3").on("mouseleave", () => {
+		$(".image3 p").css("color", "#e23a24");
 	})
 
+	$(".pic1").on("mouseenter", () => {
+		$(".pic1 p").css("color", "#bc9b07");
+	})
+	$(".pic1").on("mouseleave", () => {
+		$(".pic1 p").css("color", "#e23a24");
+	})
+	$(".pic2").on("mouseenter", () => {
+		$(".pic2 p").css("color", "#bc9b07");
+	})
+	$(".pic2").on("mouseleave", () => {
+		$(".pic2 p").css("color", "#e23a24");
+	})
+	$(".pic3").on("mouseenter", () => {
+		$(".pic3 p").css("color", "#bc9b07");
+	})
+	$(".pic3").on("mouseleave", () => {
+		$(".pic3 p").css("color", "#e23a24");
+	})
+	$(".pic4").on("mouseenter", () => {
+		$(".pic4 p").css("color", "#bc9b07");
+	})
+	$(".pic4").on("mouseleave", () => {
+		$(".pic4 p").css("color", "#e23a24");
+	})
+	$(".pic5").on("mouseenter", () => {
+		$(".pic5 p").css("color", "#bc9b07");
+	})
+	$(".pic5").on("mouseleave", () => {
+		$(".pic5 p").css("color", "#e23a24");
+	})	
+	$(".pic6").on("mouseenter", () => {
+		$(".pic6 p").css("color", "#bc9b07");
+	})
+	$(".pic6").on("mouseleave", () => {
+		$(".pic6 p").css("color", "#e23a24");
+	})	
+	$(".pic7").on("mouseenter", () => {
+		$(".pic7 p").css("color", "#bc9b07");
+	})
+	$(".pic7").on("mouseleave", () => {
+		$(".pic7 p").css("color", "#e23a24");
+	})	
+	$(".pic8").on("mouseenter", () => {
+		$(".pic8 p").css("color", "#bc9b07");
+	})
+	$(".pic8").on("mouseleave", () => {
+		$(".pic8 p").css("color", "#e23a24");
+	})	
+	$(".pic9").on("mouseenter", () => {
+		$(".pic9 p").css("color", "#bc9b07");
+	})
+	$(".pic9").on("mouseleave", () => {
+		$(".pic9 p").css("color", "#e23a24");
+	})
+	
 	$(".info2").hide();
 	$(".info3").hide();
 	$(".info4").hide();
